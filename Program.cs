b11be3fcf086650ca60d194528de61899f459f55
Gerben Heinen <gerbenheinen@gmail.com>
2017-01-05 22:49:08 +0100

    fix the problem

--- Program.cs
+++ Program.cs
@@ -13,14 +13,13 @@ namespace ReminderAPIApplication
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-                
-                
+            //var config = new ConfigurationBuilder()
+            //    .AddCommandLine(args)
+            //    .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+            //    .Build();
+
             var host = new WebHostBuilder()
-                .UseConfiguration(config)
+                //.UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
